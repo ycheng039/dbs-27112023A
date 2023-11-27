217a2ccbe94b397fb7a4827e71b535efe1392bd8
ycheng039 <142369796+ycheng039@users.noreply.github.com>
2023-11-27 16:41:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask, request, render_template
-
 app = Flask(__name__)
-
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
         rate = float(request.form.get("rate"))
-        print(rate)    
-        return(render_template("index.html",result= 90.2 +(-50.6*rate)))
+        print(rate)
+        return(render_template("index.html",result=90.2 + (-50.6*rate)))
     else:
         return(render_template("index.html", result="waiting for exchange rate............."))
-    
 if __name__ == "__main__":
     app.run()
-    
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
